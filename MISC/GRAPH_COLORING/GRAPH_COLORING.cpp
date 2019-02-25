8406b16370934b20754fc0814e2c4066612aeb4b
tluan97 <39486645+tluan97@users.noreply.github.com>
2019-02-25 14:38:43 +0700

    Revert "Update - check point"
    This reverts commit 153b098a8ca5e190860589dc249356571a5154a4.

--- MISC/GRAPH_COLORING/GRAPH_COLORING.cpp
+++ MISC/GRAPH_COLORING/GRAPH_COLORING.cpp
@@ -11,23 +11,15 @@ typedef pair<int,int> II;
 const int maxn = 1010;
 
 int n,m;
-vector<int> a[maxn];
-int aa[maxn][maxn];
+int a[maxn][maxn];
 int color[maxn];
-vector<II> d;
+vector<II> s;
 
 void input(){
 	cin>>n;
 	for (int i=1;i<=n;i++)
 	for (int j=1;j<=n;j++){
-		cin>>aa[i][j];
-	}
-	for (int i=1;i<=n;i++){
-		for (int j=1;j<=n;j++){
-			if (aa[i][j]==1){
-				a[i].push_back(j);
-			}
-		}
+		cin>>a[i][j];
 	}
 }
 void output(){
@@ -42,43 +34,24 @@ void output(){
 		cout<<"\n";
 	}
 }
-bool comp(II a,II b){
-	return a.X>b.X;
-}
 void coloring(){
-
-	for (int i=1;i<=n;i++){
-		d.push_back(II((int)a[i].size(),i));
-	}
-	sort(d.begin(),d.end(),comp);
-	int c=0;
-	for (int i=0,u;i<(int)d.size();i++){
-		u=d[i].Y;
+	for (int i=1;i<=n;i++)
+	for (int j=1;j<=n;j++)
+		a[i][0]+=a[i][j];
+	for (int i=1;i<=n;i++)
+		s.push_back(II(-a[i][0],i));
+	sort(s.begin(),s.end());
+	for (int i=0,u;i<s.size();i++){
+		u=s[i].Y;
 		if (!color[u]){
-			color[u]=++c;
-			for (int j=i+1,v;j<(int)d.size();j++){
-				v=d[j].Y;
-				if (!color[v]){
-					bool maybe=true;
-					for (int k=0;k<(int)a[v].size();k++){
-						if (c==color[a[v][k]]){
-							maybe=false;
-							break;
-						}
-					}
-					if (maybe){
-						color[v]=c;
-						// cout<<v<<" "<<color[v]<<"--\n";
-					}
-				}
+			color[u]=++m;
+			for (int j=i+1,v;j<s.size();j++){
+				v=s[j].Y;
+				if (!color[v] && !a[u][v]) 
+					color[v]=m;
 			}
-			// cout<<u<<" "<<color[u]<<"--\n";
 		}
-	}
-	// for (int i=1;i<=n;i++){
-	// 	cout<<i<<" "<<color[i]<<"\n";
-	// }
-	// cout<<c<<"\n";
+	}	
 }
 
 int main()
